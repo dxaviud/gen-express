9352c866fd545a1a57368e6ebdf2800130b56923
David Xu <62825020+dxaviud@users.noreply.github.com>
2022-04-02 16:58:05 -0400

    small changes

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const mongoose = require("mongoose");
 
 const app = express();
-const mongoose = require("mongoose");
 const uri = "mongodb://localhost:27017/local_library";
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
@@ -21,14 +18,17 @@ connection.on(
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+// middleware
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+// routes
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
+app.use("/wiki", require("./routes/wiki"));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
